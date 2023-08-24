19e87010c1079ccee8a9647874dbe7ab4cedf349
AndreiLucas123 <andrei.lucas.goncalves@gmail.com>
2023-08-23 22:10:13 -0300

    Removed support to removeImportLines

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { hoistImportDeps } from './hoistImportDeps'
 export { preserveJSX } from './preserveJSX'
 export { renameChunkPlugin } from './renameChunkPlugin'
-export { removeImportLines } from './removeImportLines'
